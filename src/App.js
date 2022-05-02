f87d211521a5a0cf1278df7c5c1c87e909752e8d
aytmamatov_musa <black.nobody.anb@gmail.com>
2022-05-02 22:15:35 +0600

    Removed unnecessary code.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Route, Switch, useHistory } from "react-router";
+import { Route, Switch } from "react-router";
 import Content from "./components/Content/Content";
 import FullCard from "./components/Content/FullCard/FullCard";
 import Favorites from "./components/Favorites/Favorites";
@@ -9,14 +7,8 @@ import Search from "./components/Navbar/Search/Search";
 import "./sass/base.sass";
 
 function App() {
-  const state = useSelector((state) => state.movie);
   return (
-    <div
-      className="App"
-      // style={{
-      //   background: `url(https://image.tmdb.org/t/p/w1920_and_h800_multi_faces${state.backdrop_path}) no-repeat 0 0 / cover`,
-      // }}
-    >
+    <div>
       <Header />
       <Search />
       <Switch>

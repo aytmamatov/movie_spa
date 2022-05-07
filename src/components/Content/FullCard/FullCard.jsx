e381fe79036186a9ebacfc2c7025b13b6d1e81a3
aytmamatov_musa <black.nobody.anb@gmail.com>
2022-05-07 17:57:35 +0600

    Removed Recommendation and ActorsCarousel component.

--- src/components/Content/FullCard/FullCard.jsx
+++ src/components/Content/FullCard/FullCard.jsx
@@ -3,19 +3,14 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router';
 import { API_KEY } from '../../../config';
 import Preloader from '../../UI/Preloader/Preloader';
-import ActorsCarousel from './ActorsCarousel/ActorsCarousel';
 import './FullCard.sass';
-import Recommendation from './Recommendation/Recommendation';
 
 function FullCard() {
   const state = useSelector((currentState) => currentState.favorites);
   const dispatch = useDispatch();
   const [movieState, setMovieState] = useState({});
   const [isLoading, setIsLoading] = useState(true);
-  const [isLoadingActors, setisLoadingActors] = useState(true);
-  const [isLoadingRecommendation, setisLoadingRecommendation] = useState(true);
   const [movieActors] = useState([]);
-  const [recommendation, setRecommendation] = useState({});
   const arrMoviesId = state.favoritesMovies.map((item) => item.id);
   const includesArrFavorites = arrMoviesId.includes(movieState.id);
 
@@ -53,16 +48,12 @@ function FullCard() {
             movieActors.push(item);
           }
         });
-      })
-      .then(() => setisLoadingActors(false));
+      });
   };
   const requestSimilarMovies = () => {
-    fetch(`https://api.themoviedb.org/3/movie/${id}/similar?api_key=${API_KEY}`)
-      .then((r) => r.json())
-      .then((r) => {
-        setRecommendation(r);
-        setisLoadingRecommendation(false);
-      });
+    fetch(`https://api.themoviedb.org/3/movie/${id}/similar?api_key=${API_KEY}`).then((r) =>
+      r.json()
+    );
   };
   const addToFavorites = (e) => {
     e.preventDefault();
@@ -88,71 +79,65 @@ function FullCard() {
       {isLoading ? (
         <Preloader />
       ) : (
-        <>
-          <div
-            className={`fullCard__inner ${
-              Object.keys(movieState).length > 0
-                ? movieState.backdrop_path !== undefined
-                  ? 'fullCard-decoration'
-                  : ''
+        <div
+          className={`fullCard__inner ${
+            Object.keys(movieState).length > 0
+              ? movieState.backdrop_path !== undefined
+                ? 'fullCard-decoration'
                 : ''
-            }`}
-            style={
-              Object.keys(movieState).length > 0
-                ? {
-                    background: `url(https://image.tmdb.org/t/p/w1920_and_h800_multi_faces${movieState.backdrop_path}) no-repeat 0 0 / cover`
+              : ''
+          }`}
+          style={
+            Object.keys(movieState).length > 0
+              ? {
+                  background: `url(https://image.tmdb.org/t/p/w1920_and_h800_multi_faces${movieState.backdrop_path}) no-repeat 0 0 / cover`
+                }
+              : null
+          }>
+          <div className="container">
+            <div className="fullCard__movie">
+              <div className="fullCard__poster">
+                <img
+                  src={
+                    movieState.poster_path !== null
+                      ? `https://image.tmdb.org/t/p/w500${movieState.poster_path}`
+                      : 'https://s3-ap-southeast-1.amazonaws.com/upcode/static/default-image.jpg'
                   }
-                : null
-            }>
-            <div className="container">
-              <div className="fullCard__movie">
-                <div className="fullCard__poster">
-                  <img
-                    src={
-                      movieState.poster_path !== null
-                        ? `https://image.tmdb.org/t/p/w500${movieState.poster_path}`
-                        : 'https://s3-ap-southeast-1.amazonaws.com/upcode/static/default-image.jpg'
-                    }
-                    alt=""
-                    className="fullCard__img"
-                  />
+                  alt=""
+                  className="fullCard__img"
+                />
+              </div>
+              <div className="fullCard__inner">
+                <h2 className="fullCard__heading">
+                  {movieState.title}
+                  <span className="fullCard__release-date"> ({releaseDateYear})</span>
+                </h2>
+                <div className="fullCard__facts">
+                  <span className="fullCard__release">{releaseDate}</span>
+                  <span className="fullCard__genres">{genres}</span>
+                  <span className="fullCard__runtime">
+                    {hourRuntime}h {minuteRuntime}m
+                  </span>
                 </div>
-                <div className="fullCard__inner">
-                  <h2 className="fullCard__heading">
-                    {movieState.title}
-                    <span className="fullCard__release-date"> ({releaseDateYear})</span>
-                  </h2>
-                  <div className="fullCard__facts">
-                    <span className="fullCard__release">{releaseDate}</span>
-                    <span className="fullCard__genres">{genres}</span>
-                    <span className="fullCard__runtime">
-                      {hourRuntime}h {minuteRuntime}m
-                    </span>
-                  </div>
-                  <span className="fullCard__tagline">{movieState.tagline}</span>
+                <span className="fullCard__tagline">{movieState.tagline}</span>
 
-                  <div className="fullCard__group">
-                    <h4 className="fullCard__overview">Overview</h4>
-                    <span className="fullCard__text">{movieState.overview}</span>
-                  </div>
-                  <div className="fullCard__actions">
-                    <button
-                      onClick={addToFavorites}
-                      className={`btn fullCard__add_to_favorites ${
-                        !includesArrFavorites ? '' : 'fullCard__disabled'
-                      }`}>
-                      Add to favorites
-                    </button>
-                  </div>
+                <div className="fullCard__group">
+                  <h4 className="fullCard__overview">Overview</h4>
+                  <span className="fullCard__text">{movieState.overview}</span>
+                </div>
+                <div className="fullCard__actions">
+                  <button
+                    onClick={addToFavorites}
+                    className={`btn fullCard__add_to_favorites ${
+                      !includesArrFavorites ? '' : 'fullCard__disabled'
+                    }`}>
+                    Add to favorites
+                  </button>
                 </div>
               </div>
             </div>
           </div>
-          <div className="container">
-            {isLoadingActors ? null : <ActorsCarousel actors={movieActors} key="ActorsCarousel" />}
-            {isLoadingRecommendation ? null : <Recommendation recommendation={recommendation} />}
-          </div>
-        </>
+        </div>
       )}
     </div>
   );

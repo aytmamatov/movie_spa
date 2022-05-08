55a968419d761faea3a8abff8dd9ee7d8ff7e93c
aytmamatov_musa <black.nobody.anb@gmail.com>
2022-05-08 11:51:07 +0600

    Reduced the async requests.

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,41 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import { getDataFromServer } from 'src/adapters/xhr';
 import Card from 'src/components/Content/Card/Card';
 import 'src/components/Content/Content.sass';
 import Preloader from 'src/components/UI/Preloader/Preloader';
-import { API_KEY } from 'src/config';
+
+const returnSearchUrl = (search, page) => `search/movie?query=${search}&page=${page}`;
+const returnPopularMoviesUrl = (page, genresIds) =>
+  `discover/movie?language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genresIds}`;
 
 function Content() {
   const state = useSelector((currentState) => currentState);
   const [movieList, setMovieList] = useState([]);
   const [movie, setMovie] = useState({});
+
   useEffect(() => {
     setMovie(state.movie);
     setMovieList(state.movie.results);
   }, [state]);
 
-  const loadMore = () => {
+  async function loadMore() {
     const currentPage = movie.page + 1;
-    if (state.movie.genresLoadMore) {
-      fetch(
-        `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${currentPage}&with_genres=${state.movie.genresIds}`
-      )
-        .then((r) => r.json())
-        .then((r) => {
-          r.results.map((item) => movieList.push(item));
-          setMovie(r);
-        });
-    } else {
-      fetch(
-        `https://api.themoviedb.org/4/search/movie?api_key=${API_KEY}&query=${state.movie.searchInfo}&page=${currentPage}`
-      )
-        .then((r) => r.json())
-        .then((r) => {
-          r.results.map((item) => movieList.push(item));
-          setMovie(r);
-        });
-    }
-  };
+    const loadMoreUrl = state.movie.genresLoadMore
+      ? returnPopularMoviesUrl(currentPage, state.movie.genresLoadMore)
+      : returnSearchUrl(state.movie.searchInfo, currentPage);
+
+    const { data: movies } = await getDataFromServer(loadMoreUrl);
+
+    setMovieList(movies.results);
+    setMovie(movies);
+  }
+
   return (
     <div className="container films">
       {state.movie.isLoading ? (

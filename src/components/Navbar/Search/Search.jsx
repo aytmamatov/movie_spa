31d5849db11f45107a6999103c9918f394540506
aytmamatov_musa <black.nobody.anb@gmail.com>
2022-05-08 23:33:10 +0600

    Implemented a search by url parameter when mounting.

--- src/components/Navbar/Search/Search.jsx
+++ src/components/Navbar/Search/Search.jsx
@@ -1,50 +1,61 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router';
+import { useSearchParams } from 'react-router-dom';
 import { getDataFromServer } from 'src/adapters/xhr';
 import 'src/components/Navbar/Search/Search.sass';
+
+const searchEmpty = {
+  search: ''
+};
 
 function Search() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-
-  const userEmpty = {
-    search: '',
-    films: '',
-    currentPage: ''
-  };
-
-  const [state, setState] = useState(userEmpty);
+  const [searchParams] = useSearchParams();
+  const [searchState, setSearchState] = useState(searchEmpty);
 
   const searchHandler = (e) => {
     navigate(`?search=${e.target.value}`);
-    localStorage.setItem('search', e.target.value);
-    setState((prev) => ({
+    setSearchState((prev) => ({
       ...prev,
       [e.target.name]: e.target.value
     }));
   };
 
-  async function submitForm(e) {
-    e.preventDefault();
+  async function sendRequest(search) {
+    const searchRequest = `search/movie?query=${search}`;
 
-    const searchRequest = `search/movie?query=${state.search}`;
-
-    if (state.search) {
-      dispatch({ type: 'SEARCH-IS-LOADING', isLoading: true });
-      try {
-        const { data: movies } = await getDataFromServer(searchRequest);
-        setState((prev) => ({ ...prev, films: movies.results, currentPage: movies.page }));
-        dispatch({
-          type: 'GET-MOVIES',
-          movie: movies,
-          search: state.search
-        });
-      } finally {
-        dispatch({ type: 'SEARCH-IS-LOADING', isLoading: false });
-      }
+    dispatch({ type: 'SEARCH-IS-LOADING', isLoading: true });
+    try {
+      const { data: movies } = await getDataFromServer(searchRequest);
+      dispatch({
+        type: 'GET-MOVIES',
+        movie: movies,
+        search
+      });
+    } finally {
+      dispatch({ type: 'SEARCH-IS-LOADING', isLoading: false });
     }
   }
+
+  function submitForm(e) {
+    e.preventDefault();
+
+    if (searchState.search) {
+      sendRequest(searchState.search);
+    }
+  }
+
+  useEffect(() => {
+    const searchParam = searchParams.get('search');
+    setSearchState((prev) => ({
+      ...prev,
+      search: searchParam
+    }));
+
+    sendRequest(searchParam);
+  }, []);
 
   return (
     <div>
